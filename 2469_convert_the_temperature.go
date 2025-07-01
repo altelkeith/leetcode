68e56fde9abe0ec7c0758004563264c10eb7f16c
altelkeith <manobaneru@yandex.ru>
2025-07-02 00:17:25 +0300

    add: new solution

---
+++ 2469_convert_the_temperature.go
@@ -0,0 +1,7 @@
+func convertTemperature(celsius float64) []float64 {
+	var kelvin, fahrenheit float64
+	kelvin = celsius + 273.15
+	fahrenheit = celsius*1.80 + 32.00
+	arr := []float64{kelvin, fahrenheit}
+	return arr
+}
